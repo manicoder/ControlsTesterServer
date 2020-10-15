4ed7772288a087b52bbc0ebd8a9524efafacce85
Manoj Kumar <manojkg@live.com>
2020-10-15 07:18:08 +0700

    Add project files.

---
+++ Hubs/SendMessageHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlsTesterServer.Hubs
+{
+    public class SendMessageHub : Hub
+    {
+        public async Task SendMessageFromServer(string values)
+        {
+            await Clients.Others.SendAsync("ReceiveNewMessage", values);
+        }
+
+        public async Task MyClassSendMessageFromServer(MyClass values)
+        {
+            //await Clients.Others.SendAsync("MyClassReceiveNewMessage", values);
+            await Clients.All.SendAsync("MyClassReceiveNewMessage", values);
+        }
+    }
+}
---
+++ MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlsTesterServer
+{
+    public class MyClass
+    {
+        public string userid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
